b775a6a729b7e3eb61cbb3c67302e6a944baa7d0
FTWinston <ftwinston@gmail.com>
2019-01-10 18:06:42 +0000

    Built project for release

---
+++ docs/precache-manifest.42673a92f6f3b78c91cae4391fa39bdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "579bee06b5e37650bdac",
+    "url": "/static/js/main.579bee06.chunk.js"
+  },
+  {
+    "revision": "f03e8ae863144a61c620",
+    "url": "/static/js/1.f03e8ae8.chunk.js"
+  },
+  {
+    "revision": "579bee06b5e37650bdac",
+    "url": "/static/css/main.44512450.chunk.css"
+  },
+  {
+    "revision": "31b606b3417b1321abcc68de215ec003",
+    "url": "/index.html"
+  }
+];
