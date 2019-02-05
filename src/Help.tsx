9031b104526ec4b3eeafe43a3fdf2808519661dd
FTWinston <ftwinston@gmail.com>
2019-02-05 18:07:46 +0000

    Updated help text

--- src/Help.tsx
+++ src/Help.tsx
@@ -20,14 +20,22 @@ export class Help extends Component<IProps, {}> {
                 </header>
 
                 <div className="site__help">
-                    Here's some basic insructions on how to hold your phone in the intended way while playing....<br/>
-                    If another way works better for you, do that instead!
-                    <ol>
-                        <li>Hold your phone in either landscape or portrait orientation, using two hands. (If you have small hands, landscape is easier.)</li>
-                        <li>Use both thumbs to push the note buttons on the edges of the screen; left hand for the left ones, right hand for the right ones.</li>
-                        <li>Use either thumb to push the central buttons, whichever one isn't in use.</li>
-                        <li>That's it. Make some music!</li>
-                    </ol>
+                    <p>
+                        The thumbophone is intended to be played on a mobile phone or similar-sized device, held in two hands.
+                        Use your left thumb for the keys on the left-hand side of the screen, and your right thumb for the keys on the right.
+                    </p>
+                    <p>
+                        The thumbophone <em>does not</em> give you access to all notes; it only lets you use notes in the selected scale. (However you can select a chromatic scale if you wish.)
+                        This is similar to how most harmonicas work, except with the thumbophone, you can quickly change the scale.
+                    </p>
+                    <p>
+                        When you first start playing, it's easiest if you ignore the smaller buttons on the outermost columns, and just use the central two columns of buttons.
+                        Once you feel comfortable with these, move on to using the outer buttons as needed.
+                    </p>
+                    <p>
+                        For advanced users, you can enable an "alternate" configuration and tilt your phone left/right to switch between configurations while playing.
+                        This can be quite offputting for new players.
+                    </p>
                 </div>
                 
                 <div className="site__links">
