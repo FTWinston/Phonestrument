d4836eb136ddf8513e5367e9d7ae3f1a805dcd58
FTWinston <ftwinston@gmail.com>
2019-11-06 00:47:40 +0000

    Converting player to a function component

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,6 +5,7 @@ import { Audio } from '../functionality/Audio';
 import './Player.css';
 import { Configure, VolumeDown, VolumeUp, KeyUp, KeyDown, OctaveDown, OctaveUp, SwitchAlt, SwitchPrimary } from './Icons';
 import { IVoice } from '../functionality/Voices';
+import { useEffect, useMemo, useState } from 'react';
 
 export interface IProfile {
     keyName: string;
@@ -27,133 +28,123 @@ interface IState extends IProfile {
     altProfile: boolean;
 }
 
-export class Player extends React.Component<IProps, IState> {
-    private audio = new Audio();
+export const Player: React.FunctionComponent<IProps> = props => {
+    const audio = useMemo(() => new Audio(), []);
 
-    constructor(props: IProps) {
-        super(props);
-        this.state = {
-            ...props.profiles[0],
-            altProfile: false,
-        };
-    }
-
-    async componentDidMount() {
-        this.audio.setVolume(this.state.volume);
-        this.audio.setVoice(this.state.voice);
-    }
-
-    componentWillUnmount() {
-        this.audio.stopAll();
-    }
-
-    componentDidUpdate(prevProps: IProps, prevState: IState) {
-        if (prevState.volume !== this.state.volume) {
-            this.audio.setVolume(this.state.volume);
-        }
-
-        if (prevState.voice !== this.state.voice) {
-            this.audio.setVoice(this.state.voice);
-        }
-    }
-
-    render() {
-        const configClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
-            e.preventDefault();
-            this.props.configure();
-        };
-
-        const noteRows = this.state.notes.map((row, index) => (
-            <div className="player__noteRow" key={index}>
-                {this.renderNoteButtons(row)}
-            </div>
-        ))
-
-        let classes = this.state.altProfile
-            ? 'player player--altProfile'
-            : 'player';
-
-        classes += ` player--${this.state.notes.length}rows`;
-
-        const profileToggle = this.props.profiles.length < 2
-            ? undefined
-            : this.state.altProfile
-                ? (
-                    <button className="playerSetting__button playerSetting__button--alt" onClick={() => this.setState({ ...this.props.profiles[0], altProfile: false })}>
-                        <SwitchPrimary />
-                    </button>
-                )
-                : (
-                    <button className="playerSetting__button playerSetting__button--alt" onClick={() => this.setState({ ...this.props.profiles[1], altProfile: true })}>
-                        <SwitchAlt />
-                    </button>
-                )
-
-        return (
-            <div className={classes}>
-                <div className="player__settings">
-                    <div className="playerSetting">
-                        <div className="playerSetting__name">Octave</div>
-                        <button className="playerSetting__button">
-                            <OctaveUp />
-                        </button>
-                        <button className="playerSetting__button">
-                            <OctaveDown />
-                        </button>
-                    </div>
-
-                    <div className="playerSetting">
-                        <div className="playerSetting__name">Volume</div>
-                        <button className="playerSetting__button">
-                            <VolumeUp />
-                        </button>
-                        <button className="playerSetting__button">
-                            <VolumeDown />
-                        </button>
-                    </div>
-
-                    <div className="playerSetting">
-                        <div className="playerSetting__name">{this.state.keyName}</div>
-                        <button className="playerSetting__button">
-                            <KeyUp />
-                        </button>
-                        <button className="playerSetting__button">
-                            <KeyDown />
-                        </button>
-                    </div>
-
-                    <div className="playerSetting">
-                        <div className="playerSetting__name">Configure</div>
-                        
-                        <button className="playerSetting__button" onClick={configClicked}>
-                            <Configure />
-                        </button>
-
-                        {profileToggle}
-                    </div>
-                </div>
-                
-                <div className="player__notes">
-                    {noteRows}
-                </div>
-            </div>
-        );
-    }
-
-    private renderNoteButtons(notes: INote[]) {
-        return notes.map((note, index) => {
-            const start = () => this.audio.start(index, note.frequency);
-            const stop = () => this.audio.stop(index);
+    const [profile, setProfile] = useState(props.profiles[0]);
     
-            return <PlayerButton
-                key={index}
-                text={note.name}
-                octave={note.octave}
-                start={start}
-                stop={stop}
-                highlight={note.name === this.state.highlightNoteName}
-                altProfile={this.state.altProfile}
-            />
-        });
-    }
+    /*
+    const [volume, setVolume] = useState(profile.volume);
+
+    const [voice, setVoice] = useState(profile.voice);
+    */
+
+    useEffect(() => {
+        return () => audio.stopAll();
+    }, []);
+
+    useEffect(() => {
+        audio.setVolume(profile.volume);
+        audio.setVoice(profile.voice);
+    }, [profile]);
+    
+    const configClicked = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        props.configure();
+    };
+
+    const noteRows = profile.notes.map((row, index) => (
+        <div className="player__noteRow" key={index}>
+            {renderNoteButtons(audio, row)}
+        </div>
+    ))
+
+    const altProfile = profile !== props.profiles[0];
+
+    let classes = altProfile
+        ? 'player player--altProfile'
+        : 'player';
+
+    classes += ` player--${profile.notes.length}rows`;
+
+    const profileToggle = props.profiles.length < 2
+        ? undefined
+        : altProfile
+            ? (
+                <button className="playerSetting__button playerSetting__button--alt" onClick={() => setProfile(props.profiles[0])}>
+                    <SwitchPrimary />
+                </button>
+            )
+            : (
+                <button className="playerSetting__button playerSetting__button--alt" onClick={() => setProfile(props.profiles[1])}>
+                    <SwitchAlt />
+                </button>
+            )
+
+    return (
+        <div className={classes}>
+            <div className="player__settings">
+                <div className="playerSetting">
+                    <div className="playerSetting__name">Octave</div>
+                    <button className="playerSetting__button">
+                        <OctaveUp />
+                    </button>
+                    <button className="playerSetting__button">
+                        <OctaveDown />
+                    </button>
+                </div>
+
+                <div className="playerSetting">
+                    <div className="playerSetting__name">Volume</div>
+                    <button className="playerSetting__button">
+                        <VolumeUp />
+                    </button>
+                    <button className="playerSetting__button">
+                        <VolumeDown />
+                    </button>
+                </div>
+
+                <div className="playerSetting">
+                    <div className="playerSetting__name">{profile.keyName}</div>
+                    <button className="playerSetting__button">
+                        <KeyUp />
+                    </button>
+                    <button className="playerSetting__button">
+                        <KeyDown />
+                    </button>
+                </div>
+
+                <div className="playerSetting">
+                    <div className="playerSetting__name">Configure</div>
+                    
+                    <button className="playerSetting__button" onClick={configClicked}>
+                        <Configure />
+                    </button>
+
+                    {profileToggle}
+                </div>
+            </div>
+            
+            <div className="player__notes">
+                {noteRows}
+            </div>
+        </div>
+    );
+}
+
+function renderNoteButtons(audio: Audio, notes: INote[]) {
+    return notes.map((note, index) => {
+        const start = () => audio.start(index, note.frequency);
+        const stop = () => audio.stop(index);
+
+        return <PlayerButton
+            key={index}
+            text={note.name}
+            octave={note.octave}
+            start={start}
+            stop={stop}
+            highlight={note.name === this.state.highlightNoteName}
+            altProfile={this.state.altProfile}
+        />
+    });
 }
